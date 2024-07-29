d35b5d52ed9cd227dc8891025a99cc2327cdbbf3
xujian <xujian.0426@foxmail.com>
2024-07-28 23:17:11 -0400

    upackage samples imgs

---
+++ scripts/unpackage_img.py
@@ -0,0 +1,23 @@
+import numpy as np
+from PIL import Image
+import os
+
+# 加载 .npz 文件
+data = np.load('/home/2021/hexin/XJ2/Log/samples_16x256x256x3.npz')
+
+# 访问数组
+array1 = data['arr_0']
+
+# 获取图片数量
+num_images = array1.shape[0]
+
+# 确定当前路径
+output_dir = '../saved_images'
+os.makedirs(output_dir, exist_ok=True)  # 创建目录（如果不存在）
+
+# 保存每一张图片
+for i in range(num_images):
+    img = Image.fromarray(array1[i].astype(np.uint8))  # 转换为 PIL 图像
+    img.save(os.path.join(output_dir, f'image_{i}.png'))  # 保存为 PNG 文件
+
+print(f"Saved {num_images} images to {output_dir}.")
